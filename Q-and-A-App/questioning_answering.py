73898b4658369a51b58f3c798ad661f2ac00bae6
Richard <rkerr022@gmail.com>
2023-11-20 19:54:28 -0500

    adding period

--- Q-and-A-App/questioning_answering.py
+++ Q-and-A-App/questioning_answering.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     st.title('OpenAI with langchain')
     st.subheader('LLM Question-Answering Application 🤖')
     with st.expander('Instructions'):
-        st.text('Use the sidebar to upload your document to the app which use ChaptGPT-4 to \nscan and answer all relevant questions. The chunk size determine the number of \nchunks and the k value determine how many chunks to view when generating \nan answer. It ideal to increase the k value to increase the chance of the AI to \ngenerate the right response')
+        st.text('Use the sidebar to upload your document to the app which use ChaptGPT-4 to \nscan and answer all relevant questions. The chunk size determine the number of \nchunks and the k value determine how many chunks to view when generating \nan answer. It ideal to increase the k value to increase the chance of the AI to \ngenerate the right response.')
     st.divider()
 
     with st.sidebar:
